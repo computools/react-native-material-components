f59c329fcad287903e4c875646268551f9bb6e38
Anastasiia Nahaiska <anastasiia.nahaiska@computools.com>
2024-01-05 18:22:41 +0200

    chore: imrove code style

--- src/icons/checkmark-icon/CheckmarkIcon.component.tsx
+++ src/icons/checkmark-icon/CheckmarkIcon.component.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import Svg, {Path} from 'react-native-svg';
 
 import {path} from './path.json';
-import type {IconProps} from '../../types/icon-props';
+import type {IconProps} from '../icon-props';
 
 const DEFAULT_SIZE = 24;
 const DEFAULT_COLOR = '#ffffff';
 
-export const CheckmarkIcon: React.FC<IconProps> = ({color, size = DEFAULT_SIZE}) => (
+export const CheckmarkIcon: React.FC<IconProps> = ({color = DEFAULT_COLOR, size = DEFAULT_SIZE}) => (
   <Svg viewBox="0 0 24 24" width={size} height={size}>
-    <Path fill={color ?? DEFAULT_COLOR} d={path} />
+    <Path fill={color} d={path} />
   </Svg>
 );

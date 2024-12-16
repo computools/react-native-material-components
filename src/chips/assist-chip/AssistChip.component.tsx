c127575edf1653320aa694f663e44ac55eb4133f
Anastasiia Nahaiska <anastasiia.nahaiska@computools.com>
2024-12-16 17:23:59 +0200

    feat: implemented assist chip

--- src/chips/assist-chip/AssistChip.component.tsx
+++ src/chips/assist-chip/AssistChip.component.tsx
@@ -8,12 +8,20 @@ import {BaseChip, type BaseChipProps} from '../base-chip/BaseChip.component';
 export interface AssistChipProps<T extends IconProps> extends BaseChipProps {
   elevated?: boolean;
   LeadingIcon?: React.FC<T>;
+  leadingIconType?: LeadingIconType;
   leadingIconProps?: T;
+}
+
+export enum LeadingIconType {
+  COMMON = 'COMMON',
+  FAVICON = 'FAVICON',
+  BRANDED = 'BRANDED',
 }
 
 export const AssistChip = <T extends IconProps>({
   elevated = false,
   LeadingIcon,
+  leadingIconType = LeadingIconType.COMMON,
   leadingIconProps = {} as T,
   style,
   labelStyle,
@@ -23,11 +31,17 @@ export const AssistChip = <T extends IconProps>({
   const theme = useTheme();
   const colorStyles = useMemo(() => getColorStyles(elevated, disabled, theme), [disabled, elevated, theme]);
 
+  const leadingIconPropsMap: Record<LeadingIconType, IconProps> = {
+    [LeadingIconType.COMMON]: {size: 18, color: colorStyles.icon.color},
+    [LeadingIconType.FAVICON]: {size: 18},
+    [LeadingIconType.BRANDED]: {size: 18, style: {opacity: 0.38}},
+  };
+
   return (
     <BaseChip
       style={[elevated && !disabled ? styles.elevatedContainer : styles.outlinedContainer, colorStyles.container, style]}
       labelStyle={[colorStyles.label, labelStyle]}
-      leadingIcon={LeadingIcon ? <LeadingIcon size={18} color={colorStyles.icon.color} {...leadingIconProps} /> : null}
+      leadingIcon={LeadingIcon ? <LeadingIcon {...leadingIconPropsMap[leadingIconType]} {...leadingIconProps} /> : null}
       {...props}
     />
   );
